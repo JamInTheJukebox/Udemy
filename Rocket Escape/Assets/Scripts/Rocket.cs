66500147ad1c21910738bba5e20bbbb7e73ac025
David Martinez <david1176@tamu.edu>
2019-07-16 00:12:06 -0700

    Update

--- Rocket Escape/Assets/Scripts/Rocket.cs
+++ Rocket Escape/Assets/Scripts/Rocket.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Vector3 RocketPosition;
+    private Rigidbody TheRocket;
+    public float thrust = 10;
+    public float torque = 10;
+    private void Awake()
+    {
+        TheRocket = GetComponent<Rigidbody>();
+    }
+    void Update ()
+    {
+        ProcessInput();
+
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            TheRocket.AddRelativeForce(thrust*Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(torque* Vector3.forward * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("TUrn Right");
+            transform.Rotate(-torque * Vector3.forward * Time.deltaTime);
+
+        }
+    }
 }
